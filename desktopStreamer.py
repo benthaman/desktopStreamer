9e03b0fcb356beadb8edec6eefd5a5d3509bf85f
Benjamin Poirier <benjamin.poirier@gmail.com>
2013-09-22 21:42:38 -0400

    Stream from parec.

--- desktopStreamer.py
+++ desktopStreamer.py
@@ -2,27 +2,59 @@
 # -*- coding: utf-8 -*-
 
 import BaseHTTPServer
- 
+import os
+import signal
+import subprocess
+import sys
+
+# Playing from the android browser, there is awful delay (~15s). This is a
+# client-side behavior. This can be confirmed by playing with "mplayer -cache
+# 32" for example.
+
+# Inspired by:
+# http://moozing.wordpress.com/2011/12/29/gstreamer-and-html5/
+# http://stackoverflow.com/questions/7502380/streaming-pulseaudio-to-file-possibly-with-gstreamer
+
+# TODO
+# * support multiple codecs
+# * the android browser seems to retry a few times when beginning to play,
+#   especially when playing silence. Investigate it...
+
 
 class AudioRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def send_audio_headers(self):
+    def do_GET(self):
+        if self.path == "/mp3":
+            content_type = "audio/mpeg"
+            encoder_args = ["lame", "-r", "-"]
+        else:
+            self.send_response(404) # http status code Not Found
+            return
+
         self.send_response(200) # http status code OK
-        self.send_header("Content-type", "audio/mpeg")
+        self.send_header("Content-type", content_type)
         self.end_headers()
 
-    def do_HEAD(self):
-        self.send_audio_headers()
+        pipe = os.pipe()
+        sp_source= subprocess.Popen(["parec", "--format=s16le",
+                                     "--device=alsa_output.pci-0000_00_1b.0.analog-stereo.monitor"],
+                                    stdout=pipe[1], stderr=sys.stderr)
+        sp_encoder= subprocess.Popen(encoder_args, stdin=pipe[0],
+                                     stdout=self.wfile, stderr=sys.stderr)
 
-    def do_GET(self):
-        self.send_audio_headers()
+        try:
+            while True:
+                (pid, tmp,) = os.wait()
+                if pid == sp_source.pid:
+                    sp_source.wait()
+                    os.kill(sp_encoder.pid, signal.SIGTERM)
+                elif pid == sp_encoder.pid:
+                    sp_encoder.wait()
+                    # source should get a sigpipe already, but it looks like
+                    # parec ignores it
+                    os.kill(sp_source.pid, signal.SIGTERM)
+        except OSError as value:
+            pass
 
-        print "handling one request"
-        f = open("/tmp/audio.mp3")
-        data = f.read()
-        self.wfile.write(data)
-        print "done"
-
- 
 
 if __name__ == "__main__":
     server_address = ('', 8081)
